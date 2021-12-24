7c5aba00f72b5cca004ee655fba19aae8647b4d0
anythingultimate <anythingultimate@gmail.com>
2021-12-25 00:21:48 +0530

    25122021

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import Axios from "axios";
 import { Fragment, useState } from "react";
 import RecipeTile from "./recipe_tile/RecipeTile";
-import CookingWoman from "./assets/woman-cook_1f469-200d-1f373.png"
-
+import CookingWoman from "./assets/woman-cook_1f469-200d-1f373.png";
 
 function App() {
   const [query, setQuery] = useState("");
   const [healthLabel, setHealthLabel] = useState("kidney-friendly");
   const [recipes, setRecipes] = useState([]);
 
-  // const url = `https://api.edamam.com/search?q=${query}&app_id=${process.env.REACT_APP_EDAMAM_ID}&app_key=${process.env.REACT_APP_EDAMAM_APP_KEY}&health=${healthLabel}`;
   const url = `https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${process.env.REACT_APP_EDAMAM_ID}&app_key=${process.env.REACT_APP_EDAMAM_APP_KEY}&health=${healthLabel}`;
 
   const getRecipeInfo = async () => {
@@ -26,7 +24,8 @@ function App() {
   return (
     <div className="app">
       <h1 onClick={getRecipeInfo}>
-        <u>Recipe Hub</u><img src={CookingWoman} alt="cooking woman" />
+        <u>Recipe Hub</u>
+        <img src={CookingWoman} alt="cooking woman" />
       </h1>
       <form className="form" onSubmit={onSubmit}>
         <div className="search__form">
@@ -89,15 +88,20 @@ function App() {
               low potassium
             </option>
           </select>
-          <input type="submit" value="Get Recipe" className="search__form__submit" />
+          <input
+            type="submit"
+            value="Get Recipe"
+            className="search__form__submit"
+          />
         </div>
       </form>
       <div className="recipes">
-        {recipes.map((recipe,i) => {
-          return <Fragment key={i}>
-          
-          <RecipeTile recipe={recipe} />
-          </Fragment>
+        {recipes.map((recipe, i) => {
+          return (
+            <Fragment key={i}>
+              <RecipeTile recipe={recipe} />
+            </Fragment>
+          );
         })}
       </div>
     </div>
--- src/recipe_tile/RecipeTile.jsx
+++ src/recipe_tile/RecipeTile.jsx
@@ -1,16 +1,19 @@
 function RecipeTile({ recipe }) {
-    return (
-        recipe["recipe"]["image"].match(/\.(jpeg|jpg|gif|png)$/) != null && (
-            <div className="recipe" onClick={() => window.open(recipe["recipe"]["url"])}>
-              <img
-                className="recipe__image"
-                src={recipe["recipe"]["image"]}
-                alt="recipe"
-              />
-              <p className="recipe__name">{recipe["recipe"]["label"]}</p>
-            </div>
-          )
+  return (
+    recipe["recipe"]["image"].match(/\.(jpeg|jpg|gif|png)$/) != null && (
+      <div
+        className="recipe"
+        onClick={() => window.open(recipe["recipe"]["url"])}
+      >
+        <img
+          className="recipe__image"
+          src={recipe["recipe"]["image"]}
+          alt="recipe"
+        />
+        <p className="recipe__name">{recipe["recipe"]["label"]}</p>
+      </div>
     )
+  );
 }
 
-export default RecipeTile
+export default RecipeTile;
